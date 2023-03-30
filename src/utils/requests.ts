2d6539239485306e9dcac1c2bf2b0178b9efc579
Andreson26 <62575538+Andreson26@users.noreply.github.com>
2023-03-30 13:00:20 -0400

    commit minor changes

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -2,7 +2,7 @@ const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 const BASE_URL = 'https://api.unsplash.com'
 
 const requests = {
-    fetchPhotos: `${BASE_URL}/photos?client_id=${API_KEY}&per_page=20`,
+    fetchPhotos: `${BASE_URL}/photos?client_id=${API_KEY}&per_page=30`,
     fetchPhoto: `${BASE_URL}/photos/:id=FHTTNcwjMEw?client_id=${API_KEY}`,
     fetchCollections: `${BASE_URL}/collections?client_id=${API_KEY}&per_page=11`,
     fetchRandomPhotos: `${BASE_URL}/photos/random?client_id=${API_KEY}&count=4`,    
